f08b95d44089f0056d7b178230f5430a50c05701
Maen Artimy <artimym@gmail.com>
2024-05-21 15:18:19 -0300

    add example

--- telemetry/gnmic_cmd.sh
+++ telemetry/gnmic_cmd.sh
@@ -8,4 +8,4 @@ shift
 OTHERS="$@"
 
 docker run --net clab --rm ghcr.io/openconfig/gnmic -a $NODE \
-       --skip-verify -u $USER -p $PASSWORD -e json_ietf $OTHERS
+--skip-verify -u $USER -p $PASSWORD -e json_ietf $OTHERS
