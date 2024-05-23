10c7f6078a53a768dd4da32bcec49abc3dbabec2
Maen Artimy <artimym@gmail.com>
2024-05-23 08:25:11 -0300

    fix

--- telemetry/generate_traffic.sh
+++ telemetry/generate_traffic.sh
@@ -14,14 +14,15 @@ set -eu
 
 startAll() {
     echo "starting traffic on all hosts"
-    docker exec host2 iperf3 -s -p 5201 -D > iperf3_1.log &
-    docker exec host2 iperf3 -s -p 5202 -D > iperf3_2.log &
+    docker exec host2 iperf3 -s -p 5201 -D
+    docker exec host2 iperf3 -s -p 5202 -D
     docker exec host1 iperf3 -c 192.168.2.11 -t 10000 -i 10 -p 5201 -B 192.168.1.11 -P 8 -b 200K -M 1460 &
     docker exec host3 iperf3 -c 192.168.2.11 -t 10000 -i 10 -p 5202 -B 192.168.3.11 -P 8 -b 200K -M 1460 &
 }
 
 stopAll() {
     echo "stopping all traffic"
+    docker exec host1 pkill iperf3
     docker exec host2 pkill iperf3
     docker exec host3 pkill iperf3
 }
